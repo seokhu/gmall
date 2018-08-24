57e6ed178bec2e235a8f55a9dc22ffa3e06085e6
tom_glb <goodMorning_pro@atguigu.com>
2018-08-24 20:21:49 +0800

    第一次test提交

---
+++ gmall-user-web/src/main/java/com/atguigu/gmall/user/GmallUesrWebApplication.java
@@ -0,0 +1,12 @@
+package com.atguigu.gmall.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmallUesrWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmallUesrWebApplication.class, args);
+	}
+}
---
+++ gmall-user-web/src/main/java/com/atguigu/gmall/user/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.atguigu.gmall.user.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.atguigu.gmall.bean.UserInfo;
+import com.atguigu.gmall.bean.UserInfo;
+import com.atguigu.gmall.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Reference
+    UserService userService;
+
+    @RequestMapping("userInfoList")
+    public ResponseEntity<List<UserInfo>> userInfoList(HttpServletRequest request){
+
+        List<UserInfo> userInfoList = userService.userInfoList();
+
+        return ResponseEntity.ok(userInfoList);
+    }
+}
